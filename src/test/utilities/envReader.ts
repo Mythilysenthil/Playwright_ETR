11e1c78faa502147b60dd1ea10e929170f17bcf0
Mythilysenthil <mythilsenthilp@gamil.com>
2026-07-16 07:12:04 +0530

    Structure modified

--- src/test/utilities/envReader.ts
+++ src/test/utilities/envReader.ts
@@ -1,11 +1,8 @@
 import * as dotenv from "dotenv";
 import * as path from "path";
 
-<<<<<<< HEAD:src/tests/utilities/envReader.ts
-export const getEnv = () => {
-=======
 export const getEnv = (): string => {
->>>>>>> 7b92136 (Added reports and hooks and world):src/test/utilities/envReader.ts
+
     const envName = process.env.ENV || "qa";
     const envPath = path.resolve( "env", `.env.${envName}`);
     console.log(`Loading environment variables from: ${envPath}`);
