70a1fd01f654ba13a390b605b4288b351dd5526d
TAMIL KUMAR E <153970943+TAMILKUMAR0027@users.noreply.github.com>
2026-07-19 11:11:14 +0530

    Update HomePage.ts

--- src/test/pages/HomePage.ts
+++ src/test/pages/HomePage.ts
@@ -6,12 +6,16 @@ export class HomePage extends BasePage{
     readonly employeeTrackRecord:Locator
     readonly filterEmployeeName:Locator
     readonly employeeNameFilter :Locator
+    readonly courseName:Locator
+    readonly courseNamesFilter:Locator
     constructor(page:Page){
         super(page);
         this.page=page;
         this.employeeTrackRecord=page.locator("//h6[@class='MuiTypography-root MuiTypography-h6 css-1rl0qlz']");
         this.filterEmployeeName=page.locator("//input[@id='_r_8_']")
         this.employeeNameFilter=page.locator("//tbody/tr/td[3]");
+        this.courseName=page.locator("//input[@id='_r_9_']")
+        this.courseNamesFilter=page.locator("//tbody/tr/td[4]")
     }
     async setEmployeeName(name: string) {
     await this.Fill(this.filterEmployeeName, name);
@@ -23,5 +27,11 @@ export class HomePage extends BasePage{
 }
    async getFilteredEmployeeNames(){
         return await this.employeeNameFilter.allTextContents();
+    }
+    async setCourseName(course:string){
+        await this.Fill(this.courseName,course)
+    }
+    async getCourseNames(){
+        return await this.courseNamesFilter.allTextContents();
     }
 }
